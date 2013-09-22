84a0f7b5f7f725b6e6db553995be1cafded30cf6
brennangw <wallaceb@grinnell.edu>
2013-09-22 14:54:09 -0500

    Did simplify, add, and multiply

---
+++ csc207-hw4/src/edu/grinnell/csc207/fletcher1/hw4/Calculator.java
@@ -0,0 +1,9 @@
+package edu.grinnell.csc207.fletcher1.hw4;
+
+public class Calculator {
+    Fraction [] memory;
+    
+    public Calculator(){
+	this.memory = new Fraction[8];
+    }//Constructor
+}
