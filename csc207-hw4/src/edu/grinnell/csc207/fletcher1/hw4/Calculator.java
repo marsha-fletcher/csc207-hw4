97a5108d2b42bf8ef5083a26dfb5bcaba04d0b13
brennangw <wallaceb@grinnell.edu>
2013-09-22 15:15:06 -0500

    FIXED THINGS

--- csc207-hw4/src/edu/grinnell/csc207/fletcher1/hw4/Calculator.java
+++
@@ -1,9 +0,0 @@
-package edu.grinnell.csc207.fletcher1.hw4;
-
-public class Calculator {
-    Fraction [] memory;
-    
-    public Calculator(){
-	this.memory = new Fraction[8];
-    }//Constructor
-}
