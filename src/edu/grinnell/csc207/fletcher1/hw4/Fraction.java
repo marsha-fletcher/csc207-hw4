854d00c1e2ceb63feb89bf7bac23b9ca29b1c3c7
Marsha Fletcher <fletcher1@grinnell.edu>
2013-09-24 17:24:09 -0500

    Update formatting to fit code conventions

--- src/edu/grinnell/csc207/fletcher1/hw4/Fraction.java
+++ src/edu/grinnell/csc207/fletcher1/hw4/Fraction.java
@@ -63,129 +63,129 @@ public class Fraction {
 	if (denom == BigInteger.ZERO) {
 	    throw new Exception("The denominator cannot be zero");
 	}
-		this.numerator = num;
-		this.denominator = denom;
-		this.simplify();
-	}// Fraction(BigInt, BigInt)
+	this.numerator = num;
+	this.denominator = denom;
+	this.simplify();
+    }// Fraction(BigInt, BigInt)
 
-	/**
-	 * Fraction(BigInteger)
-	 * 
-	 * @param num
-	 *            , a BigInteger
-	 */
-	public Fraction(BigInteger num) {
-		this.numerator = num;
-		this.denominator = BigInteger.ONE;
-	}// Fraction(BigInt)
+    /**
+     * Fraction(BigInteger)
+     * 
+     * @param num
+     *            , a BigInteger
+     */
+    public Fraction(BigInteger num) {
+	this.numerator = num;
+	this.denominator = BigInteger.ONE;
+    }// Fraction(BigInt)
 
-	/**
-	 * Fraction(long, long)
-	 * 
-	 * @param num
-	 *            , a long
-	 * @param denom
-	 *            , a non zero long
-	 * @throws Exception
-	 *             when denom = 0
-	 */
-	public Fraction(long num, long denom) throws Exception {
-		if (denom == 0) {
-			throw new Exception("The denominator cannot be zero");
-		}
-		this.numerator = BigInteger.valueOf(num);
-		this.denominator = BigInteger.valueOf(denom);
-		this.simplify();
-	}// Fraction(long, long)
+    /**
+     * Fraction(long, long)
+     * 
+     * @param num
+     *            , a long
+     * @param denom
+     *            , a non zero long
+     * @throws Exception
+     *             when denom = 0
+     */
+    public Fraction(long num, long denom) throws Exception {
+	if (denom == 0) {
+	    throw new Exception("The denominator cannot be zero");
+	}
+	this.numerator = BigInteger.valueOf(num);
+	this.denominator = BigInteger.valueOf(denom);
+	this.simplify();
+    }// Fraction(long, long)
 
-	/**
-	 * Fraction(long)
-	 * 
-	 * @param num
-	 *            , a long
-	 */
-	public Fraction(long num) {
-		this.numerator = BigInteger.valueOf(num);
-		this.denominator = BigInteger.ONE;
-	}// Fraction(long)
+    /**
+     * Fraction(long)
+     * 
+     * @param num
+     *            , a long
+     */
+    public Fraction(long num) {
+	this.numerator = BigInteger.valueOf(num);
+	this.denominator = BigInteger.ONE;
+    }// Fraction(long)
 
-	/**
-	 * Fraction(double)
-	 * 
-	 * @param num
-	 *            , a double
-	 */
-	public Fraction(double val) {
-		String valStr = String.valueOf(val);
-		int dotIndex = valStr.indexOf('.');
-		int len = valStr.length();
-		if (dotIndex < 0) { // if val doesn't have a decimal part, ie val = 5
-			this.numerator = BigInteger.valueOf((long) val);
-			this.denominator = BigInteger.ONE;
-		} else if (dotIndex > 0) {
-			BigInteger wholePart = new BigInteger(valStr.substring(0, dotIndex));
-			BigInteger decPart = new BigInteger(valStr.substring(dotIndex + 1,
-					len));
-			int decPlaces = len - (dotIndex + 1);
+    /**
+     * Fraction(double)
+     * 
+     * @param num
+     *            , a double
+     */
+    public Fraction(double val) {
+	String valStr = String.valueOf(val);
+	int dotIndex = valStr.indexOf('.');
+	int len = valStr.length();
+	if (dotIndex < 0) { // if val doesn't have a decimal part, ie val = 5
+	    this.numerator = BigInteger.valueOf((long) val);
+	    this.denominator = BigInteger.ONE;
+	} else if (dotIndex > 0) {
+	    BigInteger wholePart = new BigInteger(valStr.substring(0, dotIndex));
+	    BigInteger decPart = new BigInteger(valStr.substring(dotIndex + 1,
+		    len));
+	    int decPlaces = len - (dotIndex + 1);
 
-			this.denominator = BigInteger.TEN.pow(decPlaces);
-			this.numerator = decPart.add(wholePart.multiply(this.denominator));
-			this.simplify();
+	    this.denominator = BigInteger.TEN.pow(decPlaces);
+	    this.numerator = decPart.add(wholePart.multiply(this.denominator));
+	    this.simplify();
+	} else {
+	    this.numerator = new BigInteger(valStr.substring(1, len));
+	    this.denominator = BigInteger.TEN.multiply(BigInteger
+		    .valueOf(len - 1));
+	}
+    }// Fraction(double)
+
+    /**
+     * Fraction(string)
+     * @param val
+     * 		, a string in one of the following formats:
+     * 		"##", "#/#", "#.#", ".##" (where # is any numeral)
+     * @throws Exception 
+     * 		when the string is in the format "#/0"
+     */
+    public Fraction(String val) throws Exception {
+	int len = val.length();
+	int slashIndex = val.indexOf('/');
+	// Case where the string is in the format "#/#"
+	if (slashIndex > 0) {
+	    this.numerator = new BigInteger(val.substring(0, slashIndex));
+	    this.denominator = new BigInteger(
+		    val.substring(slashIndex + 1, len));
+	    if (this.denominator.equals(BigInteger.ZERO)) {
+		throw new Exception("The denominator cannot be zero");
+	    }// Check for denominator = 0
+	    this.simplify();
+	} else {
+	    // Case where the string is in the format "#.#"
+	    int dotIndex = val.indexOf('.');
+	    if (dotIndex >= 0) {
+		if (dotIndex == 0) {
+		    this.numerator = new BigInteger(val.substring(1, len));
+		    this.denominator = BigInteger.TEN.multiply(BigInteger
+			    .valueOf(len - 1));
 		} else {
-			this.numerator = new BigInteger(valStr.substring(1, len));
-			this.denominator = BigInteger.TEN.multiply(BigInteger
-					.valueOf(len - 1));
-		}
-	}// Fraction(double)
-	
-	/**
-	 * Fraction(string)
-	 * @param val
-	 * 		, a string in one of the following formats:
-	 * 		"##", "#/#", "#.#", ".##" (where # is any numeral)
-	 * @throws Exception 
-	 * 		when the string is in the format "#/0"
-	 */
-	public Fraction(String val) throws Exception {
-		int len = val.length();
-		int slashIndex = val.indexOf('/');
-		// Case where the string is in the format "#/#"
-		if (slashIndex > 0) {
-			this.numerator = new BigInteger(val.substring(0, slashIndex));
-			this.denominator = new BigInteger(
-					val.substring(slashIndex + 1, len));
-			if (this.denominator.equals(BigInteger.ZERO)) {
-				throw new Exception("The denominator cannot be zero");
-			}// Check for denominator = 0
-			this.simplify();
-		} else {
-			// Case where the string is in the format "#.#"
-			int dotIndex = val.indexOf('.');
-			if (dotIndex >= 0) {
-				if (dotIndex == 0) {
-					this.numerator = new BigInteger(val.substring(1, len));
-					this.denominator = BigInteger.TEN.multiply(BigInteger
-							.valueOf(len - 1));
-				} else {
-					BigInteger wholePart = new BigInteger(val.substring(0,
-							dotIndex));
-					BigInteger decPart = new BigInteger(val.substring(
-							dotIndex + 1, len));
+		    BigInteger wholePart = new BigInteger(val.substring(0,
+			    dotIndex));
+		    BigInteger decPart = new BigInteger(val.substring(
+			    dotIndex + 1, len));
 
-					int decPlaces = len - (dotIndex + 1);
-					this.denominator = BigInteger.TEN.pow(decPlaces);
-					this.numerator = decPart.add(wholePart
-							.multiply(this.denominator));
-					this.simplify();
-				}
-			} else {
-				// Else assume the string is in the format "##" (just a
-				// numerator)
-				this.numerator = new BigInteger(val);
-				this.denominator = BigInteger.ONE;
-			}
-		}// else
-	}// Fraction (String)
+		    int decPlaces = len - (dotIndex + 1);
+		    this.denominator = BigInteger.TEN.pow(decPlaces);
+		    this.numerator = decPart.add(wholePart
+			    .multiply(this.denominator));
+		    this.simplify();
+		}
+	    } else {
+		// Else assume the string is in the format "##" (just a
+		// numerator)
+		this.numerator = new BigInteger(val);
+		this.denominator = BigInteger.ONE;
+	    }
+	}// else
+    }// Fraction (String)
 
 	/***************************************************************
 	 * -----------------------Private Methods-----------------------*
@@ -195,10 +195,10 @@ public class Fraction {
 	 * this.denom is not a factor of this.numerator
 	 */
 	private void simplify() {
-		BigInteger gcd = numerator.gcd(denominator);
-		denominator = denominator.divide(gcd);
-		numerator = numerator.divide(gcd);
-		this.cleanup();
+	    BigInteger gcd = numerator.gcd(denominator);
+	    denominator = denominator.divide(gcd);
+	    numerator = numerator.divide(gcd);
+	    this.cleanup();
 	}// simplify()
 
 	/**
@@ -206,10 +206,10 @@ public class Fraction {
 	 * fraction's sign is still the same
 	 */
 	private void cleanup() {
-		if (this.denominator.compareTo(BigInteger.ZERO) < 0) {
-			this.denominator = this.denominator.negate();
-			this.numerator = this.numerator.negate();
-		}// if
+	    if (this.denominator.compareTo(BigInteger.ZERO) < 0) {
+		this.denominator = this.denominator.negate();
+		this.numerator = this.numerator.negate();
+	    }// if
 	}// cleanup
 
 	/***************************************************************
@@ -265,8 +265,8 @@ public class Fraction {
 	 * 		, the quotient of the current fraction and other (this / other)
 	 */
 	public Fraction divide(Fraction other) throws Exception{
-		return new Fraction(this.denominator.multiply(other.numerator), 
-			this.numerator.multiply(other.denominator));
+	    return new Fraction(this.denominator.multiply(other.numerator), 
+		    this.numerator.multiply(other.denominator));
 	}// divide(Fraction)
 
 	/**
@@ -276,124 +276,124 @@ public class Fraction {
 	 * 		, the result of raising this Fraction to other
 	 */
 	public Fraction pow(int other) throws Exception{
-		return new Fraction(this.numerator.pow(other), this.denominator.pow(other));
+	    return new Fraction(this.numerator.pow(other), this.denominator.pow(other));
 	}// pow(Fraction)
 
 	// OBSERVERS
 
 	public double doubleValue() {
-		return this.numerator.doubleValue() / this.denominator.doubleValue();
+	    return this.numerator.doubleValue() / this.denominator.doubleValue();
 	}// doubleValue()
 
 	public BigDecimal BigDecimalValue() {
-		return BigDecimal.valueOf(this.numerator.longValue()).divide(
-				BigDecimal.valueOf(this.denominator.longValue()));
+	    return BigDecimal.valueOf(this.numerator.longValue()).divide(
+		    BigDecimal.valueOf(this.denominator.longValue()));
 	}// BigDecimalValue()
 
-    /**
-     * Creates a new Fraction by flipping the numerator and denominator of 
-     * 	this Fraction
-     * @return 
-     * @throws Exception
-     * 		when asked to take the reciprocal of 0
-     */
-    public Fraction reciprocal() throws Exception {
-	if (this.numerator == BigInteger.valueOf(0)) {
-	    throw new Exception(
-		    "reciprocal failed: Cannot take the reciprocal of 0");
-	} else {
-	    return new Fraction(this.denominator, this.numerator);
-	} // if else to handle Exception
-    }// reciprocal()
+	/**
+	 * Creates a new Fraction by flipping the numerator and denominator of 
+	 * 	this Fraction
+	 * @return 
+	 * @throws Exception
+	 * 		when asked to take the reciprocal of 0
+	 */
+	public Fraction reciprocal() throws Exception {
+	    if (this.numerator == BigInteger.valueOf(0)) {
+		throw new Exception(
+			"reciprocal failed: Cannot take the reciprocal of 0");
+	    } else {
+		return new Fraction(this.denominator, this.numerator);
+	    } // if else to handle Exception
+	}// reciprocal()
 
-    public Fraction negate() throws Exception {
-	return new Fraction(this.numerator.multiply(BigInteger.valueOf(-1)),
-		this.denominator);
-    } // negate()
+	public Fraction negate() throws Exception {
+	    return new Fraction(this.numerator.multiply(BigInteger.valueOf(-1)),
+		    this.denominator);
+	} // negate()
 
-    public BigInteger numerator() {
-	return this.numerator;
-    }// numerator()
+	public BigInteger numerator() {
+	    return this.numerator;
+	}// numerator()
 
-    public BigInteger denominator() {
-	return this.denominator;
-    }// denominator()
+	public BigInteger denominator() {
+	    return this.denominator;
+	}// denominator()
 
-    public Fraction fractionalPart() throws Exception {
-	return new Fraction(this.numerator.mod(this.denominator),
-		this.denominator);
-    }// fractionalPart()
+	public Fraction fractionalPart() throws Exception {
+	    return new Fraction(this.numerator.mod(this.denominator),
+		    this.denominator);
+	}// fractionalPart()
 
-    public BigInteger wholePart() {
-	return this.numerator.divide(this.denominator);
-    }// wholePart()
+	public BigInteger wholePart() {
+	    return this.numerator.divide(this.denominator);
+	}// wholePart()
 
-    /***************************************************************
-     * ---------------------Standard Methods------------------------*
-     ***************************************************************/
-    /**
-     * Returns a nicely formatted string
-     */
-    public String toString() {
-	return this.numerator + "/" + this.denominator;
-    }// toString()
+	/***************************************************************
+	 * ---------------------Standard Methods------------------------*
+	 ***************************************************************/
+	/**
+	 * Returns a nicely formatted string
+	 */
+	public String toString() {
+	    return this.numerator + "/" + this.denominator;
+	}// toString()
 
-    /**
-     * Returns a new Fraction with the same numerator and denominator as 
-     * this Fraction
-     */
-    public Fraction clone() {
-	// We're using a try catch block because clone yells at us if we say
-	// throws Exception, but our constructor yells at us if we don't
-	try {
-	    return new Fraction(this.numerator, this.denominator);
-	} catch (Exception e) {
-	}
+	/**
+	 * Returns a new Fraction with the same numerator and denominator as 
+	 * this Fraction
+	 */
+	public Fraction clone() {
+	    // We're using a try catch block because clone yells at us if we say
+	    // throws Exception, but our constructor yells at us if we don't
+	    try {
+		return new Fraction(this.numerator, this.denominator);
+	    } catch (Exception e) {
+	    }
 
-	return null;
-    }// clone()
+	    return null;
+	}// clone()
 
-    /**
-     * Compares this Fraction to other
-     * @param other
-     * @return 0 if the Fractions have the same value; 
-     * 	-1 if this Fraction is less than other
-     * 	1 if this Fraction is greater than other
-     */
-    public int compareTo(Fraction other) {
-	try {
-	    return this.subtract(other).numerator.signum();
-	} catch (Exception e){
-	    return 0; //This exists because anything using our 
-	    //constructors wants us to handle exceptions
-	}
-    }// compareTo(Fraction)
+	/**
+	 * Compares this Fraction to other
+	 * @param other
+	 * @return 0 if the Fractions have the same value; 
+	 * 	-1 if this Fraction is less than other
+	 * 	1 if this Fraction is greater than other
+	 */
+	public int compareTo(Fraction other) {
+	    try {
+		return this.subtract(other).numerator.signum();
+	    } catch (Exception e){
+		return 0; //This exists because anything using our 
+		//constructors wants us to handle exceptions
+	    }
+	}// compareTo(Fraction)
 
 
-    // Added equals(Object other) to make the testing be able to compare fractions.
-    //Written by Sam Rebelsky while assisting Daniel Nanetti-Palacios
-    public boolean equals(Object other) {
-	return (other instanceof Fraction) && (this.equals((Fraction) other));
-    } // equals
+	// Added equals(Object other) to make the testing be able to compare fractions.
+	//Written by Sam Rebelsky while assisting Daniel Nanetti-Palacios
+	public boolean equals(Object other) {
+	    return (other instanceof Fraction) && (this.equals((Fraction) other));
+	} // equals
 
-    /**
-     * Checks if this Fraction is equal to other
-     * @param other
-     * @return true if the two Fractions have the same value
-     */
-    public boolean equals(Fraction other) {
-	// this should work, because Fractions are simplified upon construction
-	// and immutable
-	return (this.numerator.equals(other.numerator) && this.denominator
-		.equals(other.denominator));
-    } // equals
+	/**
+	 * Checks if this Fraction is equal to other
+	 * @param other
+	 * @return true if the two Fractions have the same value
+	 */
+	public boolean equals(Fraction other) {
+	    // this should work, because Fractions are simplified upon construction
+	    // and immutable
+	    return (this.numerator.equals(other.numerator) && this.denominator
+		    .equals(other.denominator));
+	} // equals
 
-    /**
-     * hashCode
-     * 
-     * @author Sam Rebelsky (Implementation copied from the 9/20/13 eBoard)
-     */
-    public int hashCode() {
-	return numerator.hashCode() * denominator.hashCode();
-    }// hashCode
+	/**
+	 * hashCode
+	 * 
+	 * @author Sam Rebelsky (Implementation copied from the 9/20/13 eBoard)
+	 */
+	public int hashCode() {
+	    return numerator.hashCode() * denominator.hashCode();
+	}// hashCode
 }// Fraction
